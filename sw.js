8586e7fb576d7f0423b420c67f7ee112916f00b1
Matthew Anders <suspend.still@gmail.com>
2026-09-24 17:38:51 +1000

    Turn up the Sid Basa royal archive

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_PREFIX="brta-power-ratings-";
-const CACHE_NAME=CACHE_PREFIX+"shell-v12";
+const CACHE_NAME=CACHE_PREFIX+"shell-v13";
 const SHELL_FILES=[
-  "./","index.html","site.css","future.css","style.css","archive.css","app.js?v=21","archive.js?v=3",
+  "./","index.html","site.css","future.css","style.css","archive.css?v=5","app.js?v=22","archive.js?v=4",
   "prediction.js","pwa.js","data.js","manifest.webmanifest",
   "assets/brta-logo.png","assets/brta-icon-180.png",
   "assets/brta-icon-192.png","assets/brta-icon-512.png"
